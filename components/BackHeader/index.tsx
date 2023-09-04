eeddd192c46e09dbfa2398d9a2a85d2f4836a7c7
Lucas Santos <lucassm02@gmail.com>
2023-09-03 22:57:52 -0300

    fix(BackHeader): fix text alignment

--- components/BackHeader/index.tsx
+++ components/BackHeader/index.tsx
@@ -1,6 +1,7 @@
 import { Container, Gradient, Title, ButtonContainer } from './styles'
 import { Image, TouchableOpacity } from 'react-native'
 import { useNavigation } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 
 const iconPng = require('../../assets/images/shared/back-icon.png')
@@ -12,15 +13,15 @@ export function BackHeader({ title, ...props }: Props) {
   const navigation = useNavigation()
 
   return (
-    <Container {...props}>
-      <Gradient>
+    <Gradient>
+      <Container {...props}>
         <ButtonContainer>
           <TouchableOpacity onPress={() => navigation.goBack()}>
             <Image source={iconPng} />
           </TouchableOpacity>
         </ButtonContainer>
         <Title>{title}</Title>
-      </Gradient>
-    </Container >
+      </Container >
+    </Gradient>
   )
 }
